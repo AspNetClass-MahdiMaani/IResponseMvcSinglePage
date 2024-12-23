8dd70e0bded5718174682c926cb276bd1db9e629
Mahdi7462992 <mahdimaani8@gmail.com>
2024-12-23 13:29:32 +0330

    Add project files.

---
+++ SinglePage.Sample01/Models/Services/Repositories/PersonRepository.cs
@@ -0,0 +1,123 @@
+﻿using SinglePage.Sample01.Frameworks.ResponseFrameworks;
+using SinglePage.Sample01.Frameworks.ResponseFrameworks.Contracts;
+using SinglePage.Sample01.Models.DomainModels.PersonAggregates;
+using SinglePage.Sample01.Models.Services.Contracts;
+using System.Net;
+using SinglePage.Sample01.Frameworks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Azure.Core;
+
+namespace SinglePage.Sample01.Models.Services.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly ProjectDbContext _projectDbContext;
+
+        #region [- ctor -]
+        public PersonRepository(ProjectDbContext projectDbContext)
+        {
+            _projectDbContext = projectDbContext;
+        }
+        #endregion
+
+        #region [- Insert() -]
+        public async Task<IResponse<Person>> Insert(Person model)
+        {
+            try
+            {
+                if (model is null)
+                {
+                    return new Response<Person>(false, HttpStatusCode.UnprocessableContent, ResponseMessages.NullInput, null);
+                }
+
+                await _projectDbContext.AddAsync(model);
+                await _projectDbContext.SaveChangesAsync();
+                return new Response<Person>(true, HttpStatusCode.OK, ResponseMessages.SuccessfullOperation, model);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region [- SelectAll() -]
+        public async Task<IResponse<IEnumerable<Person>>> SelectAll()
+        {
+            try
+            {
+                var persons = await _projectDbContext.Person.AsNoTracking().ToListAsync();
+                return persons is null ?
+                    new Response<IEnumerable<Person>>(false, HttpStatusCode.UnprocessableContent, ResponseMessages.NullInput, null) :
+                    new Response<IEnumerable<Person>>(true, HttpStatusCode.OK, ResponseMessages.SuccessfullOperation, persons);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region [- Select() -]
+        public async Task<IResponse<Person>> Select(Person person)
+        {
+            try
+            {
+                var responseValue = new Person();
+                if (person.Id.ToString() != "")
+                {
+                    //responseValue = await _projectDbContext.Person.FindAsync(person.Email);
+                    responseValue = await _projectDbContext.Person.Where(c => c.Email == person.Email).SingleOrDefaultAsync();
+                }
+                else
+                {
+                    responseValue = await _projectDbContext.Person.FindAsync(person.Id);
+                }
+                return responseValue is null ?
+                     new Response<Person>(false, HttpStatusCode.UnprocessableContent, ResponseMessages.NullInput, null) :
+                     new Response<Person>(true, HttpStatusCode.OK, ResponseMessages.SuccessfullOperation, responseValue);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region [- Update() -]
+        public Task<IResponse<Person>> Update(Person obj)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region [- Delete() -]
+
+        public async Task<IResponse<Person>> Delete(Person obj)
+        {
+            try
+            {
+                if (obj is null)
+                {
+                    return new Response<Person>(false, HttpStatusCode.UnprocessableContent, ResponseMessages.NullInput, null);
+                }
+                else
+                {
+                    _projectDbContext.Entry(obj).State = EntityState.Deleted;
+                    await _projectDbContext.SaveChangesAsync();
+                    return new Response<Person>(true, HttpStatusCode.OK, ResponseMessages.SuccessfullOperation, obj);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        #endregion
+    }
+}
